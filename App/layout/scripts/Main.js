f8a61de7d8a277d01a7834c6f6c2ddf8841733d4
Joseph <j-vv01@hotmail.com>
2017-06-18 01:14:57 -0600

    Todo

--- App/layout/scripts/Main.js
+++ App/layout/scripts/Main.js
@@ -9,16 +9,42 @@ function archivo(evt) {
 		}
 
 		var reader = new FileReader();
-
 		reader.onload = (function(theFile) {
 			return function(e) {
 				// Insertamos la imagen
 				document.getElementById("list").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+				NombreDocumento(theFile.name);
 			};
 		})(f);
 
 		reader.readAsDataURL(f);
 	}
+
+	TiempoSistema();
+	FechaSistema();
+}
+
+function TiempoSistema() {
+	var tiempo = new Date();
+	var hora = tiempo.getHours();
+	var minuto = tiempo.getMinutes();
+	var segundo = tiempo.getSeconds();
+
+	document.getElementById('txthora').value = hora +":" + minuto +":" + segundo;
+}
+
+function FechaSistema() {
+	var f = new Date();
+	var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+	var diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+
+	document.getElementById("txtfecha").value = diasSemana[f.getDay()] + ", " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear();
+}
+
+function NombreDocumento(Nombre){
+	document.getElementById("txtNombre").value = Nombre; 
 }
 
 document.getElementById('files').addEventListener('change', archivo, false);
+document.getElementById('txthora').addEventListener('value', TiempoSistema, false);
+document.getElementById('txtfecha').addEventListener('value', FechaSistema, false);
